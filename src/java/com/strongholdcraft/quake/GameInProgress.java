0337469867ac9da5b795dca8f95242c639448f09
Jake Wright <jake.w.96@live.co.uk>
2014-07-05 06:22:49 +0100

    Add type arguments to HashMap

--- src/java/com/strongholdcraft/quake/GameInProgress.java
+++ src/java/com/strongholdcraft/quake/GameInProgress.java
@@ -6,7 +6,7 @@ import org.bukkit.entity.Player;
 import java.util.HashMap;
 
 public class GameInProgress {
-	private static HashMap<Player, Integer> scores = new HashMap();
+	private static HashMap<Player, Integer> scores = new HashMap<Player, Integer>();
 	// private int totalkills;
 	private static Player[] players = new Player[20];
 	private static boolean inProgress;
